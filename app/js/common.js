1744093fc33346bc71e4bfdaf4dcc57063a9f13b
OMKservice <shevchuk.vitaliy@chaynx.ru>
2019-02-23 20:23:18 +0300

    1. Окончание верстки

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,18 @@
 $(function() {
 
+    window.onload = function () {
+        // Ширина input time на мобильных устройствах
+        if ($(this).width() < 768) {
+            var wdth = $('.accommodation select').outerWidth();
+            $(".accommodation input[type='time']").css("width", wdth);
+        }
+    };
+
     employmentGetInput();
 
-    $('.user-right').css("height", $('.user-left').height());
+    if ($(this).width() >= 992) {
+        $('.user-right').css("height", $('.user-left').height());
+    }
 
     $('#graph-work').mask('99/99');
 
@@ -22,8 +32,39 @@ $(function() {
 
     $('#ham8').on("click", function () {
         $(".hamburger-menu").toggleClass('active');
+        $(".main").toggle("overflow");
+        $(".footer").toggle("overflow");
     });
 
+    // Mobile city popup
+    $('#city').on("click", function () {
+       $('.cities').toggleClass('active');
+    });
+    $('#close').on("click", function () {
+        $('.cities').removeClass('active');
+    });
+    $(document).mouseup(function (e){ // событие клика по веб-документу
+        if ($('.cities').hasClass('active')) {
+            var div = $(".cities"); // тут указываем ID элемента
+            if (!div.is(e.target) // если клик был не по нашему блоку
+                && div.has(e.target).length === 0) { // и не по его дочерним элементам
+                div.removeClass('active'); // скрываем его
+            }
+        }
+    });
+
+    // Мобильная таблица
+    $('.vacancy-more').on("click", function () {
+        var nextBlock = $(this).next();
+        $(this).toggleClass('text');
+        if ($(this).hasClass('text')) {
+            $(this).text('Свернуть');
+        } else {
+            $(this).text('Узнать больше');
+        }
+        nextBlock.slideToggle();
+        $(this).toggleClass('active');
+    });
 });
 
 function employmentGetInput() {
